796e1bdf62002acb720eb6b8fd4cd52e29a97796
0ambar <abrahamr677@gmail.com>
2024-06-18 13:15:20 -0600

    Se borran las tablas intermedias para implementar mejor el ORM

--- projAds/libroStoreNet/models.py
+++ projAds/libroStoreNet/models.py
@@ -11,17 +11,18 @@ class Libro(models.Model):
     resumenLibro = models.CharField(max_length = 500, null = True, blank = True)
     precioLibro = models.DecimalField(max_digits = 7,decimal_places = 2, null = False)
     stockLibro = models.IntegerField(null = False, blank = False, default = 0)
-    descuento = models.DecimalField(max_digits = 3,decimal_places = 2, default = 0, null = True, blank = True)
-    portadalibro = models.ImageField(null = True, blank = True, upload_to='images/') 
+    descuento = models.DecimalField(max_digits = 3,decimal_places = 2, default = 0, null = True, 
+                                    blank = True)
+    portadalibro = models.ImageField(null = True, blank = True, upload_to='images/')
+    editorial = models.ForeignKey('Editorial', on_delete = models.CASCADE, 
+                                  related_name = 'librosPublicados', default = 0)
 
     def __str__(self):
         return self.tituloLibro
-    
-    # Personalización para el panel de administración de Django
+
     class Meta:
         verbose_name = 'Libro'
         verbose_name_plural = 'Libros'
-        # Nombre personalizado para la BD
         db_table = 'libro'
         ordering = ['tituloLibro']
 
@@ -54,34 +55,34 @@ class Editorial(models.Model):
 
     def __str__(self):
         return self.editorial
-    
-    # Personalización para el panel de administración de Django
+
     class Meta:
         verbose_name = 'Editorial'
         verbose_name_plural = 'Editoriales'
-        # Nombre personalizado para la BD
         db_table = 'editorial'
         ordering = ['editorial']
 
 
 """
-Modelo para relacion intermedia entre editoriales y libros
+Modelo para relacion intermedia entre transacciones y libros transaccionados
 """
-class LibroInterEditorial(models.Model):
+class TransaccionInterLibro(models.Model):
+    idTransaccion = models.ForeignKey('Transaccion', on_delete = models.CASCADE)
     idLibro = models.ForeignKey(Libro, on_delete = models.CASCADE)
-    idEditorial = models.ForeignKey(Editorial, on_delete = models.CASCADE)
-    
-    def __str__(self):
-        return f"Libro: {self.idLibro_id} - Editorial: {self.idEditorial_id}"
+    precio = models.DecimalField(max_digits = 7, decimal_places = 2, null = False)
+    piezas = models.PositiveIntegerField(null = False, blank = False)
 
-    # Personalización para el panel de administración de Django
+    def __str__(self):
+        return f"Transaccion: {self.idTransaccion} - Libro: {self.idLibro}"
+
+    # # Personalización para el panel de administración de Django
     class Meta:
         # Nombre personalizado para la BD
-        db_table = 'libro_editorial'
+        db_table = 'transaccion_libro'
         # Se asegura que la combinacion de las FKs sea unica
         constraints = [
-            models.UniqueConstraint(fields = ['idLibro', 'idEditorial'], 
-                                    name = 'unique_libro_editorial')
+            models.UniqueConstraint(fields = ['idTransaccion', 'idLibro'], 
+                                    name = 'unique_transaccion_libro')
         ]
 
 
@@ -90,9 +91,12 @@ Modelo para transacciones
 """
 class Transaccion(models.Model):
     tiempoTransaccion = models.DateTimeField(auto_now_add = True, unique = True)
-    precioTransaccion = models.DecimalField(max_digits = 7,decimal_places = 2, null = False)
     totalTransaccion = models.DecimalField(max_digits = 10,decimal_places = 2, null = False)
-    piezasTransaccion = models.PositiveIntegerField(null = False)
+    libros = models.ManyToManyField(Libro, through = TransaccionInterLibro)
+    tipoTransaccion = models.ForeignKey('TipoTransaccion', on_delete = models.DO_NOTHING, 
+                                        default = 0)
+    empleado = models.ForeignKey('Empleado', on_delete = models.DO_NOTHING, default = 0)
+    cliente = models.ForeignKey('Cliente', on_delete = models.DO_NOTHING, default = 0) 
 
     def __str__(self):
         return f"Fecha y hora: {self.tiempoTransaccion} - Total: {self.totalTransaccion}"
@@ -104,27 +108,6 @@ class Transaccion(models.Model):
         # Nombre personalizado para la BD
         db_table = 'transaccion'
         ordering = ['tiempoTransaccion']
-
-
-"""
-Modelo para relacion intermedia entre transacciones y libros transaccionados
-"""
-class TransaccionInterLibro:
-    idTransaccion = models.ForeignKey(Transaccion, on_delete = models.CASCADE)
-    idLibro = models.ForeignKey(Libro, on_delete = models.CASCADE)
-
-    def __str__(self):
-        return f"Transaccion: {self.idTransaccion} - Libro: {self.idLibro}"
-
-    # Personalización para el panel de administración de Django
-    class Meta:
-        # Nombre personalizado para la BD
-        db_table = 'transaccion_libro'
-        # Se asegura que la combinacion de las FKs sea unica
-        constraints = [
-            models.UniqueConstraint(fields = ['idTransaccion', 'idLibro'], 
-                                    name = 'unique_transaccion_libro')
-        ]
 
 
 """
@@ -146,27 +129,6 @@ class TipoTransaccion(models.Model):
 
 
 """
-Modelo para relacion intermedia entre transaccion y tipo de transaccion
-"""
-class TransaccionInterTipoTransaccion(models.Model):
-    idTransaccion = models.ForeignKey(Transaccion, on_delete = models.CASCADE)
-    idTipoTransaccion = models.ForeignKey(TipoTransaccion, on_delete = models.CASCADE)
-    
-    def __str__(self):
-        return f"Transaccion: {self.idTransaccion} - Tipo: {self.idTipoTransaccion}"
-
-    # Personalización para el panel de administración de Django
-    class Meta:
-        # Nombre personalizado para la BD
-        db_table = 'transaccion_tipoTransaccion'
-        # Se asegura que la combinacion de las FKs sea unica
-        constraints = [
-            models.UniqueConstraint(fields = ['idTransaccion', 'idTipoTransaccion'], 
-                                    name = 'unique_transaccion_tipoTransaccion')
-        ]
-
-
-"""
 Modelo para un empleado de tienda 
 """
 class Empleado(models.Model):
@@ -175,7 +137,6 @@ class Empleado(models.Model):
     apellidoMaternoEmpleado = models.CharField(max_length = 50, blank = True, null = True)
     usuarioEmpleado = models.CharField(max_length = 9, null = False)
     contraseniaEmpleado = models.CharField(max_length = 128, null = False)
-    tipoEmpleado = models.CharField(max_length = 3, null = False)
     turnoEmpleado = models.CharField(max_length = 1, null = False)
     estatusEmpleado = models.CharField(max_length = 1, null = False)
 
@@ -192,59 +153,21 @@ class Empleado(models.Model):
 
 
 """
-Modelo para relacion intermedia entre empleados y transacciones
-"""
-class EmpleadoInterTransaccion(models.Model):
-    idEmpleado = models.ForeignKey(Empleado, on_delete = models.CASCADE)
-    idTransaccion = models.ForeignKey(Transaccion, on_delete = models.CASCADE)
-    
-    def __str__(self):
-        return f"Empleado: {self.idEmpleado} - Transaccion: {self.idTransaccion}"
-
-    # Personalización para el panel de administración de Django
-    class Meta:
-        # Nombre personalizado para la BD
-        db_table = 'empleado_transaccion'
-        # Se asegura que la combinacion de las FKs sea unica
-        constraints = [
-            models.UniqueConstraint(fields = ['idEmpleado', 'idTransaccion'], 
-                                    name = 'unique_empleado_transaccion')
-        ]
-
-
-"""
 Modelo para corte de caja
 """
 class CorteCaja(models.Model):
     tiempoCorte = models.DateTimeField(auto_now_add = True, unique = True)
+    empleados = models.ForeignKey(Empleado, default = 0, on_delete = models.DO_NOTHING)
 
     def __str__(self):
         return str(self.tiempoCorte)
     
     class Meta:
+        verbose_name = 'Corte de Caja'
+        verbose_name_plural = 'Cortes de Caja'
         # Nombre personalizado para la BD
         db_table = 'corteCaja'
-
-
-"""
-Modelo para relacion intermedia entre corte de caja y empleado
-"""
-class CorteCajaInterEmpleado(models.Model):
-    idCorteCaja = models.ForeignKey(CorteCaja, on_delete = models.CASCADE)
-    idEmpleado = models.ForeignKey(Empleado, on_delete = models.CASCADE)
-    
-    def __str__(self):
-        return f"Empleado: {self.idEmpleado} - Corte: {self.idCorteCaja}"
-
-    # Personalización para el panel de administración de Django
-    class Meta:
-        # Nombre personalizado para la BD
-        db_table = 'corteCaja_empleado'
-        # Se asegura que la combinacion de las FKs sea unica
-        constraints = [
-            models.UniqueConstraint(fields = ['idEmpleado', 'idCorteCaja'], 
-                                    name = 'unique_corteCaja_empleado')
-        ]
+        ordering = ['tiempoCorte']
 
 
 """
@@ -252,6 +175,7 @@ Modelo para tipo de empleado
 """
 class TipoEmpleado(models.Model):
     descripcionTipoEmpleado = models.CharField(max_length = 20, null = False, unique = True)
+    empleados = models.ManyToManyField(Empleado, related_name = 'tipoEmpleados')
 
     def __str__(self):
         return self.descripcionTipoEmpleado
@@ -260,27 +184,7 @@ class TipoEmpleado(models.Model):
     class Meta:
         # Nombre personalizado para la BD
         db_table = 'tipoEmpleado'
-
-
-"""
-Modelo para relacion intermedia entre empleado y tipo de empleado
-"""
-class EmpleadoInterTipoEmpleado(models.Model):
-    idEmpleado = models.ForeignKey(Empleado, on_delete = models.CASCADE)
-    idTipoEmpleado = models.ForeignKey(TipoEmpleado, on_delete = models.CASCADE)
-    
-    def __str__(self):
-        return f"Empleado: {self.idEmpleado} - Tipo: {self.idTipoEmpleado}"
-
-    # Personalización para el panel de administración de Django
-    class Meta:
-        # Nombre personalizado para la BD
-        db_table = 'empleado_tipoEmpleado'
-        # Se asegura que la combinacion de las FKs sea unica
-        constraints = [
-            models.UniqueConstraint(fields = ['idEmpleado', 'idTipoEmpleado'], 
-                                    name = 'unique_empleado_tipoEmpleado')
-        ]
+        ordering = ['descripcionTipoEmpleado']
 
 
 """
@@ -308,27 +212,6 @@ class Cliente(models.Model):
 
 
 """
-Modelo para relacion intermedia entre transaccion y un cliente de la pagina web
-"""
-class ClienteInterTransaccion(models.Model):
-    idCliente = models.ForeignKey(Cliente, on_delete = models.CASCADE)
-    idTransaccion = models.ForeignKey(Transaccion, on_delete = models.CASCADE)
-    
-    def __str__(self):
-        return f"Cliente: {self.idCliente} - Transaccion: {self.idTransaccion}"
-
-    # Personalización para el panel de administración de Django
-    class Meta:
-        # Nombre personalizado para la BD
-        db_table = 'cliente_transaccion'
-        # Se asegura que la combinacion de las FKs sea unica
-        constraints = [
-            models.UniqueConstraint(fields = ['idTransaccion', 'idCliente'], 
-                                    name = 'unique_cliente_transaccion')
-        ]
-
-
-"""
 Modelo para el domicilio de un cliente
 """
 class Domicilio(models.Model):
@@ -336,6 +219,10 @@ class Domicilio(models.Model):
     numeroExteriorDomicilio = models.CharField(max_length = 10, null = False, default = "s/n")
     codigoPostalDomicilio = models.CharField(max_length = 5, blank = False, null = False)
     coloniaDomicilio = models.CharField(max_length = 50, null = False)
+    municipioDomicilio = models.CharField(max_length = 50, null = False, default = 0)
+    estadoDomicilio = models.CharField(max_length = 50, null = False, default = 0)
+    cliente = models.ForeignKey(Cliente, on_delete = models.CASCADE, default = 0,
+                                related_name = 'domicilio')
 
     def __str__(self):
         return f"{self.calleDomicilio}, Num: {self.numeroExteriorDomicilio}, {self.codigoPostalDomicilio}"
@@ -347,70 +234,6 @@ class Domicilio(models.Model):
         # Nombre personalizado para la BD
         db_table = 'domicilio'
         ordering = ['codigoPostalDomicilio']
-
-
-"""
-Modelo para relacion intermedia entre cliente y domicilio
-"""
-class ClienteInterDomicilio(models.Model):
-    idCliente = models.ForeignKey(Cliente, on_delete = models.CASCADE)
-    idDomicilio = models.ForeignKey(Domicilio, on_delete = models.CASCADE)
-    
-    def __str__(self):
-        return f"Cliente: {self.idCliente} - Domicilio: {self.idDomicilio}"
-
-    # Personalización para el panel de administración de Django
-    class Meta:
-        # Nombre personalizado para la BD
-        db_table = 'cliente_domicilio'
-        # Se asegura que la combinacion de las FKs sea unica
-        constraints = [
-            models.UniqueConstraint(fields = ['idDomicilio', 'idCliente'], 
-                                    name = 'unique_cliente_domicilio')
-        ]
-
-
-"""
-Modelo para direcciones
-"""
-class Direccion(models.Model):
-    codigoPostal = models.CharField(max_length = 5, null = False)
-    asentamiento = models.CharField(max_length = 50, null = False)
-    mnpio = models.CharField(max_length = 50, null = False)
-    estado = models.CharField(max_length = 50, null = False)
-    # Identificador de asentamiento (un codigo postal puede tener varios asentamientos)
-    idAsentaCodigoPostal = models.CharField(max_length = 5, null = False)
-
-    def __str__(self):
-        return f"{self.codigoPostal} {self.asentamiento}, {self.mnpio}, {self.estado}"
-    
-    class Meta:
-        verbose_name = 'Direccion'
-        verbose_name_plural = 'Direcciones'
-        # Nombre personalizado para la BD
-        db_table = 'direccion'
-        ordering = ['codigoPostal', 'asentamiento']
-
-
-"""
-Modelo para relacion intermedia entre domicilio y direcciones
-"""
-class DireccionInterDomicilio(models.Model):
-    idDireccion = models.ForeignKey(Direccion, on_delete = models.CASCADE)
-    idDomicilio = models.ForeignKey(Domicilio, on_delete = models.CASCADE)
-    
-    def __str__(self):
-        return f"Direccion: {self.idDireccion} - Domicilio: {self.idDomicilio}"
-
-    # Personalización para el panel de administración de Django
-    class Meta:
-        # Nombre personalizado para la BD
-        db_table = 'Direccion_domicilio'
-        # Se asegura que la combinacion de las FKs sea unica
-        constraints = [
-            models.UniqueConstraint(fields = ['idDomicilio', 'idDireccion'], 
-                                    name = 'unique_Direccion_domicilio')
-        ]
 
 
 """
