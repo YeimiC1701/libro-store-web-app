41393fc7d1a0e83a839117d41c26f99d5721f2ec
YeimiC1701 <95209620+YeimiC1701@users.noreply.github.com>
2024-07-03 07:38:43 -0600

    libroDetalle js
    Se creo el js de los detalles del libro

---
+++ projAds/libroStoreNet/static/js/libroDetalle.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $('#cantidad').val('0')
+  $('#decremento').click(function() {
+    let currentValue = $('#cantidad').val();
+    if (currentValue < 1 || isNaN(currentValue)) {
+      $('#cantidad').val(0);
+    }
+      currentValue = parseInt($('#cantidad').val());
+      if (currentValue >= 1) {
+          $('#cantidad').val(currentValue - 1);
+      }
+  });
+
+  $('#incremento').click(function() {
+    let currentValue = $('#cantidad').val();
+    if (currentValue < 1 || isNaN(currentValue)) {
+      $('#cantidad').val(0);
+    }
+    currentValue = parseInt($('#cantidad').val());
+      $('#cantidad').val(currentValue + 1);
+  });
+
+
+
+
+});
